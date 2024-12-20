1eaa4b32daf770e41db5cdb61e24a302fa075711
Quiban <161413262+QuibanDev@users.noreply.github.com>
2024-12-20 16:59:16 +0800

    Delete nav directory

--- nav/js/contact.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(window).on("scroll", function () {
-      if ($(this).scrollTop() > 80) {
-        $("header").addClass("sticky");
-      } else {
-        $("header").removeClass("sticky");
-      }
-    });
-  });
